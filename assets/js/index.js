2c727631e321d154faff86b25f7999dd515fdb85
ki33520 <ki33520@gmail.com>
2016-11-09 17:10:02 +0800

    fix

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,4 +9,29 @@ require(["jquery","swiper"],function($,swiper) {
         paginationClickable: true,
         loop: true
     });
+    
+	$('.add-sub').each(function(){
+		var sub = $(this).find('.btn-sub'),
+			add = $(this).find('.btn-add'),
+			number = $(this).find('.number'),
+			num = Number(number.text()),
+			max = $(this).data('max');
+		function count(){
+			num = num + this;
+			sub.removeClass('disabeld');
+			add.removeClass('disabeld');
+			if(num<=0){
+				num = 0;
+				sub.addClass('disabeld');
+			}
+			if(num>=max){
+				num = max;
+				add.addClass('disabeld');
+			}
+			number.text(num);
+		}
+		sub.on('click',count.bind(-1));
+		add.on('click',count.bind(1));
+	})
+
 });
